806a077727311c4ba8a7c32ba32dcc961b5c7958
Artem-Usachev <bbbiii.444@yandex.ru>
2021-03-12 00:04:52 +0500

    revie1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,3 @@
-// open Popup
 let openButton = document.querySelector('.info__setting-box');
 let closeButton = document.querySelector('.popup__exit-img');
 let popup = document.querySelector('.popup');
@@ -18,12 +17,54 @@ closeButton.addEventListener('click', closePopup);
 
 
 
-// write nex text
+
+let saveButton = document.querySelector('.popup__button')
 let inputUserName = document.querySelector('.popup__userName');
 let inputUserInfo = document.querySelector('.popup__userInfo');
 let userName = document.querySelector('.info__title');
 let userInfo = document.querySelector('.info__subtitle');
-console.log(inputUserName.textContent);
-console.log(userInfo.textContent);
 
-inputUserName.insertAdjacentText('afterbegin', 'hello');
+// equalization userName inputUserName
+inputUserName.value = userName.innerText;
+
+// equalization userInfo inputUserInfo
+inputUserInfo.value = userInfo.innerText;
+
+// receiving ValueName 
+let inputValueName = '';
+
+function infoValueHandler(e) {
+    inputValueInfo = e.target.value;
+}
+
+
+//  receiving ValueInfo
+let inputValueInfo = '';
+
+function nameValueHandler(e) {
+
+    inputValueName = e.target.value;
+}
+
+// conclusion Value in userName
+function clickHeandlerName(e) {
+
+    e.preventDefault();
+    userName.textContent = inputValueName;
+
+}
+//  conclusion Value in userInfo
+
+function clickHeandlerInfo(e) {
+
+    e.preventDefault();
+    userInfo.textContent = inputValueInfo;
+
+}
+
+
+inputUserName.addEventListener('change', (e) => nameValueHandler(e));
+saveButton.addEventListener('click', (e) => clickHeandlerName(e));
+
+inputUserInfo.addEventListener('change', (e) => infoValueHandler(e));
+saveButton.addEventListener('click', (e) => clickHeandlerInfo(e));
