d65a3d0c0a66d85305a466849c26130ebe0d43ba
Artem-Usachev <77751845+Artem-Usachev@users.noreply.github.com>
2021-03-11 21:53:10 +0500

    открытие попапа

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// open Popup
 let openButton = document.querySelector('.info__setting-box');
 let closeButton = document.querySelector('.popup__exit-img');
 let popup = document.querySelector('.popup');
@@ -14,3 +15,15 @@ function closePopup() {
 }
 
 closeButton.addEventListener('click', closePopup);
+
+
+
+// write nex text
+let inputUserName = document.querySelector('.popup__userName');
+let inputUserInfo = document.querySelector('.popup__userInfo');
+let userName = document.querySelector('.info__title');
+let userInfo = document.querySelector('.info__subtitle');
+console.log(inputUserName.textContent);
+console.log(userInfo.textContent);
+
+inputUserName.insertAdjacentText('afterbegin', 'hello');
