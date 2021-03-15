14d45206264d4fd9be7f705eaab26172654c5fb7
Artem-Usachev <77751845+Artem-Usachev@users.noreply.github.com>
2021-03-15 16:35:38 +0500

    revie2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,60 +1,30 @@
-let popup = document.querySelector('.popup');
 let openButton = document.querySelector('.info__setting-box');
+let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__exit-img');
-
+let inputUserName = popup.querySelector('input[name="name"]');
+let inputUserInfo = popup.querySelector('input[name="job"]');
+let userName = document.querySelector('.info__title');
+let userInfo = document.querySelector('.info__subtitle');
 
 function openPopup() {
-    popup.classList.remove('invisible');
     inputUserName.value = userName.innerText;
     inputUserInfo.value = userInfo.innerText;
+    popup.classList.remove('invisible');
 }
 
 function closePopup() {
     popup.classList.add('invisible');
 }
 
-let saveButton = document.querySelector('.popup__button')
-let inputUserName = document.querySelector('.popup__user-name');
-let inputUserInfo = document.querySelector('.popup__user-info');
-let userName = document.querySelector('.info__title');
-let userInfo = document.querySelector('.info__subtitle');
-
-// receiving ValueName 
-let inputValueName = '';
-
-function infoValueHandler(e) {
-    inputValueInfo = e.target.value;
-}
-
-//  receiving ValueInfo
-let inputValueInfo = '';
-
-function nameValueHandler(e) {
-    inputValueName = e.target.value;
-}
-
-// conclusion Value in userName
-function clickHeandlerName(e) {
-
-    e.preventDefault();
-    userName.textContent = inputValueName;
-
-}
-//  conclusion Value in userInfo
-
 function clickHeandlerInfo(e) {
 
     e.preventDefault();
-    userInfo.textContent = inputValueInfo;
-
+    userInfo.textContent = inputUserInfo.value;
+    userName.textContent = inputUserName.value;
+    closePopup();
 
 }
+
 openButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-
-
-inputUserName.addEventListener('change', (e) => nameValueHandler(e));
-saveButton.addEventListener('submit', (e) => clickHeandlerName(e));
-
-inputUserInfo.addEventListener('change', (e) => infoValueHandler(e));
-saveButton.addEventListener('submit', (e) => clickHeandlerInfo(e));
+popup.addEventListener('submit', (e) => clickHeandlerInfo(e));
