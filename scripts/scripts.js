538b52ab7639641cd5e16537baaadb91afcad55d
Artem-Usachev <77751845+Artem-Usachev@users.noreply.github.com>
2021-04-11 22:26:00 +0500

    work3revie1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 // work1
 const openButton = document.querySelector('.info__setting-box');
 const popupProfile = document.querySelector('.popup_type_profile');
-const popup = document.querySelector('.popup');
+const popup = document.querySelectorAll('.popup');
 const inputUserName = popupProfile.querySelector('input[name="name"]');
 const inputUserInfo = popupProfile.querySelector('input[name="job"]');
 const userName = document.querySelector('.info__title');
@@ -9,6 +9,8 @@ const userInfo = document.querySelector('.info__subtitle');
 
 function openPopup(popupElement) {
     popupElement.classList.remove('invisible');
+    addListenerEsc();
+    addListenerClickPopup()
 }
 
 function closePopup(popupElement) {
@@ -136,3 +138,90 @@ function addNewCard(e) {
 }
 popupPlaceForm.addEventListener('submit', addNewCard);
 renderCards();
+
+// work3
+const forms = document.querySelectorAll('.popup__content');
+const formInputes = document.querySelectorAll('.popup__text');
+const placeBtn = document.getElementById('btn-place');
+const profileBtn = document.getElementById('btn-profile');
+const popupProfileInpur = document.querySelector('.popup_type_profile')
+const popupAddPlace = document.querySelector('.popup_type_place')
+const inputListProfile = Array.from(popupProfileInpur.querySelectorAll('.popup__text'));
+const inputListPlace = Array.from(popupAddPlace.querySelectorAll('.popup__text'));
+
+function hideError(inputId) {
+    const errorElement = document.querySelector(`.${inputId}-error`);
+    const input = document.getElementById(inputId);
+    input.classList.remove('popup__error');
+    errorElement.classList.remove('visible');
+    errorElement.classList.add('invisible');
+}
+
+function showError(inputId, message) {
+    const errorElement = document.querySelector(`.${inputId}-error`);
+    const input = document.getElementById(inputId);
+    input.classList.add('popup__error');
+    errorElement.textContent = message;
+    errorElement.classList.remove('invisible');
+    errorElement.classList.add('visible');
+}
+
+function validateInputValue(input) {
+    if (input.validity.valid) {
+        hideError(input.id);
+        return;
+    }
+    showError(input.id, input.validationMessage);
+}
+
+function addValidateListeners(inputes) {
+    inputes.forEach((input) => {
+        input.addEventListener('input', (e) => {
+            validateInputValue(e.currentTarget)
+            toggleButtonState(inputListPlace, placeBtn)
+            toggleButtonState(inputListProfile, profileBtn)
+        });
+    });
+}
+
+function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+}
+
+function toggleButtonState(inputList, button) {
+
+    if (hasInvalidInput(inputList)) {
+        button.setAttribute('disabled', "disabled")
+        button.classList.add('popup__button_condition_disable');
+        button.classList.remove('popup__button_condition_active');
+    } else {
+        button.removeAttribute('disabled')
+        button.classList.remove('popup__button_condition_disable');
+        button.classList.add('popup__button_condition_active');
+    }
+
+};
+
+function addListenerEsc() {
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode === 27) {
+            closePopup(popupPlace)
+            closePopup(popupProfile)
+            closePopup(photoPopup)
+        }
+    })
+}
+
+function addListenerClickPopup() {
+    popup.forEach((popup) => {
+        popup.addEventListener('click', function(e) {
+            if (e.target.classList.contains('popup')) {
+                closePopup(e.target);
+            }
+        });
+    })
+}
+
+addValidateListeners(formInputes);
