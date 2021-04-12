a0ae0295c52b4f2781aa53e63947c99203b39bb2
Artem-Usachev <bbbiii.444@yandex.ru>
2021-04-12 15:59:59 +0500

    3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -123,7 +123,6 @@ openAddCardPopupBtn.addEventListener('click', function() {
 });
 popupPlaceForm.addEventListener('submit', addNewCard);
 renderCards();
-<<<<<<< HEAD
 //work3
 
 function removeButtonVisible(button) {
@@ -169,87 +168,10 @@ function createClickPopupClosing(e) {
     if (e.target.classList.contains('popup')) {
         closePopup(e.target);
     }
-=======
-
-// work3
-const forms = document.querySelectorAll('.popup__content');
-const formInputes = document.querySelectorAll('.popup__text');
-const placeBtn = document.getElementById('btn-place');
-const profileBtn = document.getElementById('btn-profile');
-const popupProfileInpur = document.querySelector('.popup_type_profile')
-const popupAddPlace = document.querySelector('.popup_type_place')
-const inputListProfile = Array.from(popupProfileInpur.querySelectorAll('.popup__text'));
-const inputListPlace = Array.from(popupAddPlace.querySelectorAll('.popup__text'));
-
-function hideError(inputId) {
-    const errorElement = document.querySelector(`.${inputId}-error`);
-    const input = document.getElementById(inputId);
-    input.classList.remove('popup__error');
-    errorElement.classList.remove('visible');
-    errorElement.classList.add('invisible');
-}
-
-function showError(inputId, message) {
-    const errorElement = document.querySelector(`.${inputId}-error`);
-    const input = document.getElementById(inputId);
-    input.classList.add('popup__error');
-    errorElement.textContent = message;
-    errorElement.classList.remove('invisible');
-    errorElement.classList.add('visible');
-}
-
-function validateInputValue(input) {
-    if (input.validity.valid) {
-        hideError(input.id);
-        return;
-    }
-    showError(input.id, input.validationMessage);
-}
-
-function addValidateListeners(inputes) {
-    inputes.forEach((input) => {
-        input.addEventListener('input', (e) => {
-            validateInputValue(e.currentTarget)
-            toggleButtonState(inputListPlace, placeBtn)
-            toggleButtonState(inputListProfile, profileBtn)
-        });
-    });
-}
-
-function hasInvalidInput(inputList) {
-    return inputList.some((inputElement) => {
-        return !inputElement.validity.valid;
-    });
-}
-
-function toggleButtonState(inputList, button) {
-
-    if (hasInvalidInput(inputList)) {
-        button.setAttribute('disabled', "disabled")
-        button.classList.add('popup__button_condition_disable');
-        button.classList.remove('popup__button_condition_active');
-    } else {
-        button.removeAttribute('disabled')
-        button.classList.remove('popup__button_condition_disable');
-        button.classList.add('popup__button_condition_active');
-    }
-
-};
-
-function addListenerEsc() {
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode === 27) {
-            closePopup(popupPlace)
-            closePopup(popupProfile)
-            closePopup(photoPopup)
-        }
-    })
->>>>>>> 86fbfd19df03a8093d7a0fde8545fc6296d3d66b
 }
 
 function addListenerClickPopup() {
     popup.forEach((popup) => {
-<<<<<<< HEAD
         popup.addEventListener('click', e => createClickPopupClosing(e));
     })
 }
@@ -259,14 +181,3 @@ function removeListenerClickPopup() {
         popup.removeEventListener('click', e => createClickPopupClosing(e));
     })
 }
-=======
-        popup.addEventListener('click', function(e) {
-            if (e.target.classList.contains('popup')) {
-                closePopup(e.target);
-            }
-        });
-    })
-}
-
-addValidateListeners(formInputes);
->>>>>>> 86fbfd19df03a8093d7a0fde8545fc6296d3d66b
