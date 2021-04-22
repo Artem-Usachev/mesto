a81ac3194a6b8915cf9a3469795c82b00894fed8
Artem-Usachev <77751845+Artem-Usachev@users.noreply.github.com>
2021-04-23 00:30:27 +0500

    revie1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
-import { Card } from './Card'
-import { FormValidator } from './FormValidator'
+import { Card } from './Card.js'
+import { FormValidator } from './FormValidator.js'
 
 const valid = new FormValidator(validationConfig);
 valid.enableValidation()
